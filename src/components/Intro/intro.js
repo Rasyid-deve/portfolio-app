a46b88a2222b1089485540f7b7a016384a588ad2
Rasyid-deve <renaldirasyed@gmail.com>
2023-12-20 06:50:26 +0700

    updated hero

---
+++ src/components/Intro/intro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './intro.css';
+
+const Intro = () => {
+  return <section id="intro">
+    <div className="introComment"></div>
+  </section>;
+};
+
+export default Intro;
