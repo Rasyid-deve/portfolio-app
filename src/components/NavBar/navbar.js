0b828e1f6def6bd73f9f487de7ed67fd2991567d
Rasyid-deve <renaldirasyed@gmail.com>
2023-12-19 22:21:56 +0700

    add several photo and library

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import './navbar.css';
+import R_logo from '../../assets/R_logo.png';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <img src={R_logo} alt="Logo" className="logo" />
+      <div className="desktopMenu">
+        <Link></Link>
+      </div>
+      <button className="">
+        <img src="" alt="" className="desktopMenuImg" />
+        Contact Me
+      </button>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
