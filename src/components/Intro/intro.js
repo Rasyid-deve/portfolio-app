57fef06da96702ca436e7d3f38a9a690a71defb5
Rasyid-deve <renaldirasyed@gmail.com>
2023-12-20 21:37:44 +0700

    updated

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './intro.css';
+import bg from '../../assets/image.png';
+import btnImage from '../../assets/hireme.png';
+import { Link } from 'react-scroll';
 
 const Intro = () => {
-  return <section id="intro">
-    <div className="introComment"></div>
-  </section>;
+  return (
+    <section id="intro">
+      <div className="introContent">
+        <span className="hello">Hello,</span>
+        <span className="introText">
+          I'm <span className="introName">Rasyed</span> <br />
+          Backend Developer
+        </span>
+        <p className="introPara">
+          I am an enthusiastic Backend developer experienced in the Javascript. <br />
+          have a experience using the NodeJS framework and also have experience <br />
+          using MySQL and MongoDB.{' '}
+        </p>
+        <Link>
+          <button className="btn">
+            <img src={btnImage} alt="Hire" className="btnImg" />
+            Hire Me
+          </button>
+        </Link>
+      </div>
+      <img src={bg} alt="Profile" className="bg" />
+    </section>
+  );
 };
 
 export default Intro;
