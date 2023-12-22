a03f0fa60accf30f2576c738c3145f1192c1e85f
Rasyid-deve <renaldirasyed@gmail.com>
2023-12-22 08:20:20 +0700

    footer updated

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './footer.css';
 
 const Footer = () => {
-  return <div>footer</div>;
+  return <footer className="footer">Copyright &#169; 2023 Rasyed Renaldi. All right received.</footer>;
 };
 
 export default Footer;
