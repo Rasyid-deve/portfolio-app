5c1f248320982fa470be29bb3791458200092b6d
Rasyid-deve <renaldirasyed@gmail.com>
2023-12-22 08:11:42 +0700

    update contact page

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import './contact.css';
+import Walmart from '../../assets/walmart.png';
+import Adobe from '../../assets/adobe.png';
+import Microsoft from '../../assets/microsoft.png';
+import Facebook from '../../assets/facebook.png';
+import FacebookIcon from '../../assets/facebook-icon.png';
+import TwitterIcon from '../../assets/twitter.png';
+import YoutubeIcon from '../../assets/youtube.png';
+import InstagramIcon from '../../assets/instagram.png';
 
 const Contact = () => {
-  return <div>My Contact</div>;
+  return (
+    <section id="contactPage">
+      <div id="clients">
+        <h1 className="contactPageTitle">My Client</h1>
+        <p className="clientDesc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat ratione ducimus cupiditate, numquam consectetur aliquam?</p>
+        <div className="clientImgs">
+          <img src={Walmart} alt="Client" className="clientImg" />
+          <img src={Adobe} alt="Client" className="clientImg" />
+          <img src={Microsoft} alt="Client" className="clientImg" />
+          <img src={Facebook} alt="Client" className="clientImg" />
+        </div>
+      </div>
+      <div id="contact">
+        <h1 className="contactPageTitle">Contact Me</h1>
+        <span className="contactDesc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam, corporis.</span>
+        <form className="contactForm">
+          <input type="text" className="name" placeholder="Your Name" />
+          <input type="email" className="email" placeholder="Your Email" />
+          <textarea name="message" rows="5" className="msg" placeholder="Your Message"></textarea>
+          <button type="submit" value="Send" className="submitBtn">
+            Submit
+          </button>
+          <div className="links">
+            <img src={FacebookIcon} alt="Facebook" className="link" />
+            <img src={TwitterIcon} alt="Twitter" className="link" />
+            <img src={YoutubeIcon} alt="YoutubeIcon" className="link" />
+            <img src={InstagramIcon} alt="Instagram" className="link" />
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
